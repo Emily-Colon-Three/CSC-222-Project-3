4dd635fea9dd613dc71dc45113df85df20097fdb
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-03-17 23:05:45 -0400

    Added 3 new member functions for calculations.
    getMin(), getMax(), and getAverage() implemented.

--- NumberArrayProgram/NumberArray.cpp
+++ NumberArrayProgram/NumberArray.cpp
@@ -21,3 +21,54 @@ NumberArray::~NumberArray()
     std::cout << "Memory de-allocated.\n"; // Prints message confirming memory release
 }
 
+void NumberArray::setNumber(int i, double val)
+{
+    if (i < NumberArray::size && i >= 0)
+    {
+        NumberArray::data[i] = val; // Only sets the value if index is valid, avoiding undefined behavior
+    }
+}
+
+double NumberArray::getMin() const
+{
+    double min = NumberArray::data[0]; // Minimum value initialized to very first index, before the rest of the search finds the true minimum, if it is not the lowest.
+
+    for (int i = 1; i < NumberArray::size; i++) // Starts at 1 because 0 already checked
+    {
+        if (NumberArray::data[i] < min)
+        {
+            min = NumberArray::data[i];
+        }
+    }
+
+    return min; // Once the minimum is found, full array searched, it is returned.
+}
+
+double NumberArray::getMax() const
+{
+    double max = NumberArray::data[0]; // Max first set to the initial value
+
+    for (int i = 1; i < NumberArray::size; i++)
+    {
+        if (NumberArray::data[i] > max)
+        {
+            max = NumberArray::data[i]; // If larger than the max, the element becomes new maximum value
+        }
+    }
+
+    return max;
+}
+
+double NumberArray::getAverage() const
+{
+    double average = 0;
+
+    for (int i = 0; i < NumberArray::size; i++)
+    {
+        average += NumberArray::data[i]; // Value is added to average
+    }
+
+    average /= NumberArray::size; // Average divided by size of array, the number of values in it.
+
+    return average; // Average, now found, is returned.
+}
